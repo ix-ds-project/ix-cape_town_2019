5c8076db55f248e1304ee7d5b00769e147bd13d2
PenguinsGoYun <yeonjunkim99@gmail.com>
2019-07-25 14:51:51 +0200

    Cleaned up NA data

--- visualization.R
+++ visualization.R
@@ -6,15 +6,40 @@ library(lubridate)
 # Read in the csv into a dataframe
 df <- data.frame(read.csv('data/raw/teaching_training_data.csv'))
 
-# Filter out the "relevant" columns
-clean_df <- select(df, X, survey_num, survey_date_month, gender, dob, working)
+# Find all NA values per column
+# colSums(is.na(df))
 
+# CLEAN------------------------------------------------------------------------
 # Filter out the first survey data
-first_survey_data <- filter(clean_df, survey_num==1)
-# Clean up rows containing NA values
-first_survey_data <- na.omit(first_survey_data)
+first_survey_data <- filter(df, survey_num==1)
+# Filter out the "relevant" columns
+first_survey_data <- select(first_survey_data, X, survey_date_month, gender, dob, working, volunteer, leadershiprole, peoplelive, peoplelive_15plus, numchildren, numearnincome, anygrant, anyhhincome)
 
-# Check number of NAs per column
+# Replace all NAs accordingly
+first_survey_data[is.na(first_survey_data$anygrant),]$anygrant <- FALSE
+first_survey_data[is.na(first_survey_data$anyhhincome),]$anyhhincome <- FALSE
+first_survey_data[is.na(first_survey_data$volunteer),]$volunteer <- "No"
+first_survey_data[is.na(first_survey_data$peoplelive_15plus),]$peoplelive_15plus <- 0
+first_survey_data[is.na(first_survey_data$peoplelive),]$peoplelive <- 0
+first_survey_data[is.na(first_survey_data$leadershiprole),]$leadershiprole <- "No"
+first_survey_data[is.na(first_survey_data$numchildren),]$numchildren <- 0
+first_survey_data[is.na(first_survey_data$numearnincome),]$numearnincome <- 0
+
+# Remove "0: I live alone"
+first_survey_data[ ((first_survey_data$peoplelive=="0: I live alone") & (first_survey_data$numearnincome=="0: I live alone")), ]$peoplelive <- 0
+first_survey_data[ ((first_survey_data$peoplelive==0) & (first_survey_data$numearnincome=="0: I live alone")), ]$numearnincome <- 0
+first_survey_data[ ((first_survey_data$peoplelive=="0: I live alone") & (first_survey_data$numearnincome==0)), ]$peoplelive <- 0
+first_survey_data <- filter(first_survey_data, !(peoplelive=="0: I live alone" | numearnincome=="0: I live alone"))
+
+# Remove "15 or More"
+first_survey_data <- filter(first_survey_data, !(peoplelive=="More than 15" | peoplelive_15plus=="More than 15" | numearnincome=="More than 15"))
+first_survey_data <- filter(first_survey_data, !(peoplelive=="15 or more" | peoplelive_15plus=="15 or more" | numearnincome=="15 or more"))
+
+# Remove all remaining rows containing NAs
+colSums(is.na(first_survey_data))
+first_survey_data <- first_survey_data[!is.na(first_survey_data$gender),]
+first_survey_data <- first_survey_data[!is.na(first_survey_data$dob),]
+# first_survey_data <- na.omit(first_survey_data, cols="gender")
 colSums(is.na(first_survey_data))
 
 # Create "age" column based on date of birth
@@ -22,13 +47,21 @@ first_survey_data <- first_survey_data %>%
   mutate(age_at_survey = interval(dob, survey_date_month)/years(1)) %>%
   mutate(age = floor(age_at_survey))
 
-head(first_survey_data, n=20)
+# Drop column
+first_survey_data <- select(first_survey_data, -age_at_survey)
+#------------------------------------------------------------------------------
 
+# VISUALIZATION----------------------------------------------------------------
 # Histogram of female working vs male working
 ggplot(first_survey_data, aes(x=working, fill=gender)) + 
   geom_histogram(stat="count")
 
 # Histogram of age with working divided by gender
-ggplot(first_survey_data, aes(x=age, fill=working)) +
+ggplot(first_survey_data, aes(x=peoplelive, fill=working)) +
   geom_histogram(binwidth = 1) +
   facet_grid(gender ~ .)
+
+
+
+
+
