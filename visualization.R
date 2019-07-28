903334a4aa34917fbfb5f19961f5fb49b857b0ef
PenguinsGoYun <yeonjunkim99@gmail.com>
2019-07-28 22:37:23 +0200

    Added model attempts

--- visualization.R
+++ visualization.R
@@ -2,65 +2,264 @@ library(ggplot2)
 library(tidyverse)
 library(dplyr)
 library(lubridate)
+library(caret)
+library(skimr)
+library(RANN)
+library(ISLR)
+library(pastecs)
+library(lattice)
 
 # Read in the csv into a dataframe
 df <- data.frame(read.csv('data/raw/teaching_training_data.csv'))
+df_cft <- data.frame(read.csv('data/raw/teaching_training_data_cft.csv'))
+df_com <- data.frame(read.csv('data/raw/teaching_training_data_com.csv'))
+df_grit <- data.frame(read.csv('data/raw/teaching_training_data_grit.csv'))
+df_num <- data.frame(read.csv('data/raw/teaching_training_data_num.csv'))
+df_opt <- data.frame(read.csv('data/raw/teaching_training_data_opt.csv'))
 
 # Find all NA values per column
 # colSums(is.na(df))
 
 # CLEAN------------------------------------------------------------------------
 # Filter out the first survey data
-first_survey_data <- filter(df, survey_num==1)
-# Filter out the "relevant" columns
-first_survey_data <- select(first_survey_data, X, survey_date_month, gender, dob, working, volunteer, leadershiprole, peoplelive, peoplelive_15plus, numchildren, numearnincome, anygrant, anyhhincome)
+fsd <- filter(df, survey_num==1)
 
-# Replace all NAs accordingly
-first_survey_data[is.na(first_survey_data$anygrant),]$anygrant <- FALSE
-first_survey_data[is.na(first_survey_data$anyhhincome),]$anyhhincome <- FALSE
-first_survey_data[is.na(first_survey_data$volunteer),]$volunteer <- "No"
-first_survey_data[is.na(first_survey_data$peoplelive_15plus),]$peoplelive_15plus <- 0
-first_survey_data[is.na(first_survey_data$peoplelive),]$peoplelive <- 0
-first_survey_data[is.na(first_survey_data$leadershiprole),]$leadershiprole <- "No"
-first_survey_data[is.na(first_survey_data$numchildren),]$numchildren <- 0
-first_survey_data[is.na(first_survey_data$numearnincome),]$numearnincome <- 0
+colSums(is.na(fsd))
+# Filter out the "irrelevant" columns
+fsd <- select(fsd, -job_start_date, -job_leave_date, -company_size, -monthly_pay, -survey_num, -province)
+# Filter out error rows (repeated IDs)
+fsd <- distinct(fsd, unid, .keep_all = TRUE)
 
-# Remove "0: I live alone"
-first_survey_data[ ((first_survey_data$peoplelive=="0: I live alone") & (first_survey_data$numearnincome=="0: I live alone")), ]$peoplelive <- 0
-first_survey_data[ ((first_survey_data$peoplelive==0) & (first_survey_data$numearnincome=="0: I live alone")), ]$numearnincome <- 0
-first_survey_data[ ((first_survey_data$peoplelive=="0: I live alone") & (first_survey_data$numearnincome==0)), ]$peoplelive <- 0
-first_survey_data <- filter(first_survey_data, !(peoplelive=="0: I live alone" | numearnincome=="0: I live alone"))
+colSums(is.na(df))
+colSums(is.na(fsd))
 
-# Remove "15 or More"
-first_survey_data <- filter(first_survey_data, !(peoplelive=="More than 15" | peoplelive_15plus=="More than 15" | numearnincome=="More than 15"))
-first_survey_data <- filter(first_survey_data, !(peoplelive=="15 or more" | peoplelive_15plus=="15 or more" | numearnincome=="15 or more"))
-
-# Remove all remaining rows containing NAs
-colSums(is.na(first_survey_data))
-first_survey_data <- first_survey_data[!is.na(first_survey_data$gender),]
-first_survey_data <- first_survey_data[!is.na(first_survey_data$dob),]
-# first_survey_data <- na.omit(first_survey_data, cols="gender")
-colSums(is.na(first_survey_data))
+# Remove rows with NA
+fsd <- fsd[!is.na(fsd$gender),]
+fsd <- fsd[!is.na(fsd$dob),]
 
 # Create "age" column based on date of birth
-first_survey_data <- first_survey_data %>%
+fsd <- fsd %>%
   mutate(age_at_survey = interval(dob, survey_date_month)/years(1)) %>%
   mutate(age = floor(age_at_survey))
 
-# Drop column
-first_survey_data <- select(first_survey_data, -age_at_survey)
+# Drop columns
+fsd <- select(fsd, -age_at_survey, -dob, -survey_date_month, -financial_situation_now, -financial_situation_5years)
+
+# Cleaning function
+helper_function <- function(file_name) {
+  file_name %>%
+    select(2:3) %>%
+    distinct(unid, .keep_all=TRUE)
+}
+df_cft <- helper_function(df_cft)
+df_com <- helper_function(df_com)
+df_grit <- helper_function(df_grit)
+df_num <- helper_function(df_num)
+df_opt <- helper_function(df_opt)
+
+# JOIN all the dataframe
+fsd <- left_join(fsd, df_cft, by="unid")
+fsd <- left_join(fsd, df_com, by="unid")
+fsd <- left_join(fsd, df_grit, by="unid")
+fsd <- left_join(fsd, df_num, by="unid")
+fsd <- left_join(fsd, df_opt, by="unid")
+
+colSums(is.na(fsd))
+
+fsd <- mutate(fsd,
+              # Male=1, Female=0
+              gender = factor(ifelse(gender=="Male", 1, 0)),
+              
+              # Yes=1, No=0
+              volunteer = factor(ifelse(volunteer=="Yes", 1, 0)),
+              leadershiprole = factor(ifelse(leadershiprole=="Yes", 1, 0)),
+              
+              working = factor(ifelse(working==TRUE, 1, 0)),
+              
+              # Clean data to numeric
+              peoplelive = parse_number(as.character(peoplelive)),
+              peoplelive_15plus= parse_number(as.character(peoplelive_15plus)),
+              numchildren = parse_number(as.character(numchildren)),
+              numearnincome = parse_number(as.character(numearnincome))
+)
+
+# Median imputing NAs
+# fsd$working <- as.factor(make.names(fsd$working))
+preProcValues <- preProcess(fsd, method = c("medianImpute"))
+fsd_processed <- predict(preProcValues, fsd)
+
+colSums(is.na(fsd_processed))
+
+glimpse(fsd)
+glimpse(fsd_processed)
+
+# # Filter out responses with NAs in these columns
+# fsd <- filter(fsd, !(is.na(volunteer) & 
+#                        is.na(leadershiprole) & 
+#                        is.na(peoplelive) & 
+#                        is.na(peoplelive_15plus) & 
+#                        is.na(numchildren) &
+#                        is.na(numearnincome) &
+#                        is.na(financial_situation_now) & 
+#                        is.na(financial_situation_5years) & 
+#                        is.na(givemoney_yes)))
+# 
+# # Create new column "Responsive" based on NAs
+# fsd <- mutate(fsd,
+#               responsive = factor(ifelse(!(is.na(volunteer) & 
+#                                              is.na(leadershiprole) & 
+#                                              is.na(peoplelive) & 
+#                                              is.na(peoplelive_15plus) & 
+#                                              is.na(numchildren) &
+#                                              is.na(numearnincome) &
+#                                              is.na(financial_situation_now) & 
+#                                              is.na(financial_situation_5years) & 
+#                                              is.na(givemoney_yes)), 1, 0))
+# )
+# 
+# # Convert "0: I live alone" to just "0"
+# fsd[ ((fsd$peoplelive=="0: I live alone") & (fsd$numearnincome=="0: I live alone")), ]$peoplelive <- 0
+# fsd[ ((fsd$peoplelive==0) & (fsd$numearnincome=="0: I live alone")), ]$numearnincome <- 0
+# fsd[ ((fsd$peoplelive=="0: I live alone") & (fsd$numearnincome==0)), ]$peoplelive <- 0
+# fsd <- filter(fsd, !(peoplelive=="0: I live alone" | numearnincome=="0: I live alone"))
+# 
+# fsd[ (fsd$peoplelive=="More than 15"), ]$peoplelive <- 16
+# fsd[ (fsd$peoplelive_15plus=="More than 15"), ]$peoplelive_15plus <- 16
+# fsd[ (fsd$peoplelive=="15 or more"), ]$peoplelive <- 16
+# fsd[ (fsd$peoplelive_15plus=="15 or more"), ]$peoplelive_15plus <- 16
+# 
+# fsd[ (!(fsd$peoplelive < fsd$peoplelive_15plus)), ]
+# 
+# # Replace all NAs accordingly
+# fsd[is.na(fsd$anygrant),]$anygrant <- FALSE
+# fsd[is.na(fsd$anyhhincome),]$anyhhincome <- FALSE
+# fsd[is.na(fsd$volunteer),]$volunteer <- "No"
+# fsd[is.na(fsd$peoplelive_15plus),]$peoplelive_15plus <- 0
+# fsd[is.na(fsd$peoplelive),]$peoplelive <- 0
+# fsd[is.na(fsd$leadershiprole),]$leadershiprole <- "No"
+# fsd[is.na(fsd$numchildren),]$numchildren <- 0
+# fsd[is.na(fsd$numearnincome),]$numearnincome <- 0
+# 
+# # Remove "15 or More"
+# fsd <- filter(fsd, !(peoplelive=="More than 15" | peoplelive_15plus=="More than 15" | numearnincome=="More than 15"))
+# fsd <- filter(fsd, !(peoplelive=="15 or more" | peoplelive_15plus=="15 or more" | numearnincome=="15 or more"))
+# 
+# # Remove all remaining rows containing NAs
+# colSums(is.na(fsd))
+# fsd <- fsd[!is.na(fsd$gender),]
+# fsd <- fsd[!is.na(fsd$dob),]
+# # fsd <- na.omit(fsd, cols="gender")
+# colSums(is.na(fsd))
+# 
+# 
+# glimpse(fsd)
 #------------------------------------------------------------------------------
+
+
 
 # VISUALIZATION----------------------------------------------------------------
 # Histogram of female working vs male working
-ggplot(first_survey_data, aes(x=working, fill=gender)) + 
+ggplot(fsd, aes(x=working, fill=gender)) + 
   geom_histogram(stat="count")
 
 # Histogram of age with working divided by gender
-ggplot(first_survey_data, aes(x=peoplelive, fill=working)) +
+ggplot(fsd, aes(x=age, fill=working)) +
   geom_histogram(binwidth = 1) +
   facet_grid(gender ~ .)
+#------------------------------------------------------------------------------
 
+
+
+# MODELING---------------------------------------------------------------------
+# fsd_processed = select(fsd_processed, gender, working, volunteer, leadershiprole, age, peoplelive, peoplelive_15plus)
+# 
+# fsd_processed <- mutate(fsd_processed,
+#                 # Male=1, Female=0
+#                 gender = factor(ifelse(gender=="Male", 1, 0)),
+# 
+#                 # Yes=1, No=0
+#                 volunteer = factor(ifelse(volunteer=="Yes", 1, 0)),
+#                 leadershiprole = factor(ifelse(leadershiprole=="Yes", 1, 0)),
+# 
+#                 working = factor(ifelse(working, 1, 0)),
+# 
+#                 peoplelive = as.numeric(levels(peoplelive))[peoplelive],
+#                 peoplelive_15plus = as.numeric(levels(peoplelive_15plus))[peoplelive_15plus]
+# )
+# 
+# 
+# 
+# glimpse(fsd)
+# 
+# colSums(is.na(fsd))
+# 
+# dummies_model <- dummyVars(working ~ . , data=fsd)
+# 
+# fsd_working <- fsd$working
+# fsd_mat <- predict(dummies_model, newdata = fsd)
+# 
+# fsd <- data.frame(fsd_mat)
+# 
+# head(fsd)
+# 
+# preProcess_scale_model <- preProcess(fsd[c('age', 'peoplelive', 'peoplelive_15plus')], method=c('center', 'scale'))
+# 
+# fsd[c('age', 'peoplelive', 'peoplelive_15plus')] <- predict(preProcess_scale_model, newdata = fsd[c('age', 'peoplelive', 'peoplelive_15plus')])
+# 
+# fsd$working <- fsd_working
+# 
+# 
+# set.seed(100)
+
+temp <- fsd_processed
+fsd_processed <- select(fsd_processed, -volunteer, -leadershiprole, -anygrant, -anyhhincome, -givemoney_yes)
+
+colSums(is.na(fsd_processed))
+
+# Step 1: Get row numbers for the training data
+trainRowNumbers <- createDataPartition(fsd_processed$working, p=0.8, list=FALSE)
+# Step 2: Create the training  dataset
+trainData <- fsd_processed[trainRowNumbers,]
+# Step 3: Create the test dataset
+testData <- fsd_processed[-trainRowNumbers,]
+
+## Defining the training controls for multiple models
+trControl <- trainControl(method = "cv",
+                          number = 5,
+                          verboseIter = TRUE)
+
+# Model RANGER
+model_ranger <- train(working ~ ., trainData,
+                      method='ranger',
+                      trControl=trControl,
+                      metric="Accuracy")
+
+testData$pred_ranger <- predict(object=model_ranger, testData)
+confusionMatrix(testData$pred_ranger, testData$working)
+
+# Model RANGER with weights
+# Model weights
+model_weights <- ifelse(trainData$working == 0,
+                        (1/table(trainData$working)[1]) * 0.5,
+                        (1/table(trainData$working)[2]) * 0.5)
+
+model_ranger_w <- train(working ~ ., trainData,
+                      method='ranger',
+                      trControl=trControl,
+                      weights = model_weights,
+                      metric="Accuracy")
+
+testData$pred_ranger_w <- predict(object=model_ranger_w, testData)
+confusionMatrix(testData$pred_ranger_w, testData$working)
+
+# Model XGBTREE
+model_xgbTree <- train(working ~ ., trainData,
+                      method='xgbTree',
+                      trControl=trControl,
+                      metric="Accuracy")
+
+testData$pred_xgbTree <- predict(object=model_xgbTree, testData)
+confusionMatrix(testData$pred_xgbTree, testData$working)
 
 
 
